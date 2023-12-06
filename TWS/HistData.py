754f6267e07cb2c25c62f4e29530081a69bae804
ljp2 <ljp2@lehigh.edu>
2023-12-05 21:28:44 -0500

    Histworking

--- TWS/HistData.py
+++ TWS/HistData.py
@@ -5,15 +5,16 @@ from ibapi.contract import Contract
 from ibapi.common import BarData
 import threading
 import time
+from datetime import datetime
 
 class TradingApp(EWrapper, EClient):
-    def __init__(self, event_Done:threading.Event):
+    def __init__(self):
         EClient.__init__(self, self)
-        self.event_done = event_Done
+        self.event_done = threading.Event()
         self.bars = []
 
-    def historicalData(self, reqId, bar):
-        self.bars.append( {'Date': bar.date, 'Open': bar.open, 'High': bar.high, 'Low': bar.low, 'Close': bar.close, 'Volume': bar.volume} )
+    def historicalData(self, reqId, bar:BarData):
+        self.bars.append( {'Date': bar.date.rsplit(' ', 1)[0], 'Open': bar.open, 'High': bar.high, 'Low': bar.low, 'Close': bar.close, 'Volume': bar.volume} )
 
         
     def historicalDataEnd(self, reqId: int, start: str, end: str):
@@ -23,37 +24,61 @@ class TradingApp(EWrapper, EClient):
 def websocket_con():
     app.run()    
 
-event_done:threading.Event = threading.Event()
-app = TradingApp(event_done)
-app.connect("127.0.0.1", 7497, clientId=1)
-con_thread = threading.Thread(target=websocket_con, daemon=True)
-con_thread.start()
-time.sleep(1)
 
-contract = Contract()
-contract.symbol = "ES"
-contract.secType = "FUT"
-contract.exchange = "CME"
-contract.currency = "USD"
-contract.lastTradeDateOrContractMonth = "202403"
+def getBarsOneDayOneMinute(end_date:str, contract_month:str):
+    global app
+    app.event_done.clear()
 
-app.reqHistoricalData(
-    reqId=1,
-    contract=contract,
-    endDateTime="",
-    durationStr="1 D",
-    barSizeSetting="30 mins",
-    whatToShow="TRADES",
-    useRTH=1,
-    formatDate=1,
-    keepUpToDate=0,
-    chartOptions = []
-)
+    contract = Contract()
+    contract.symbol = "ES"
+    contract.secType = "FUT"
+    contract.exchange = "CME"
+    contract.currency = "USD"
+    contract.lastTradeDateOrContractMonth = contract_month
 
-event_done.wait()
-df:pd.DataFrame = pd.DataFrame(app.bars)
-print(df)
+    # endDateTime=''
+    endDateTime=f'{end_date}-23:59:59'
+    # endDateTime='20231130-23:59:59'
+    print('endDateTime', endDateTime)
+
+    app.reqHistoricalData(
+        reqId=1,
+        contract=contract,
+        endDateTime=endDateTime,
+        durationStr="1 D",
+        barSizeSetting="30 mins",
+        whatToShow="TRADES",
+        useRTH=1,
+        formatDate=1,
+        keepUpToDate=0,
+        chartOptions = []
+    )
+
+    app.event_done.wait()
+    df:pd.DataFrame = pd.DataFrame(app.bars)
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+
+    return df;
 
 
-app.disconnect()
-print("MAIN DONE")
+
+def main():
+    global app
+    app = TradingApp()
+    app.connect("127.0.0.1", 7497, clientId=1)
+    con_thread = threading.Thread(target=websocket_con, daemon=True)
+    con_thread.start()
+    time.sleep(1)
+
+
+
+    df = getBarsOneDayOneMinute(end_date='20231130', contract_month='202403')
+    print(df)
+
+
+    app.disconnect()
+    print("MAIN DONE")
+
+
+main()
