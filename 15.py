ab69278faa68cc8cbca32836b74649f90aafd1d8
ljp2 <ljp2@lehigh.edu>
2023-12-09 20:01:22 -0500

    working

--- 15.py
+++ 15.py
@@ -1,4 +1,8 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
+import mplfinance as mpf 
+
+from multiprocessing import Process
 
 def ohlc(df:pd.DataFrame):
     x = [{
@@ -7,27 +11,54 @@ def ohlc(df:pd.DataFrame):
         'Low' : df.Low.min(),
         'Close' : df.Close.iloc[-1]
     }]
-    zf =  pd.DataFrame(x, index=[df.index[0]])
-    return zf
+    return pd.DataFrame(x, index=[df.index[0]])
 
-df = pd.read_csv('c:/Data/20231130.csv', index_col=0, parse_dates=True)
 
-print(df.head(7))
+def combineDF(df:pd.DataFrame, group_sz:int) -> pd.DataFrame:
+    xf = pd.DataFrame()
+    numdfrows = df.shape[0]
+    ngroups = numdfrows // group_sz
+    numtargetrows = ngroups * group_sz
+    start = numdfrows - numtargetrows
+    while start < numdfrows:
+        end = start + group_sz
+        xf = pd.concat(
+            [xf, ohlc(df.iloc[start:end])]
+        )
+        start += group_sz
+    return xf
 
-inc = 3
 
-s = df.shape[0]
-ngroups= s // inc
-N = ngroups * inc
-first = s - N
+def multibars(df:pd.DataFrame, group_sz:int) -> pd.DataFrame:
+    xf = pd.DataFrame()
+    N = df.shape[0]
+    start = 0
+    end = start + group_sz
+    while end <= N:
+        xf = pd.concat(
+            [xf, ohlc(df.iloc[start:end])]
+        )
+        start += 1
+        end += 1
+    return xf
 
-xf = pd.DataFrame()
-start = first
-last = start + inc
-while start < s:
-    wf = ohlc(df.iloc[start:last])
-    xf = pd.concat([xf, wf])  #, ignore_index=True)
-    start += inc
-    last += inc
 
-print(xf.head(ngroups))
+
+def PlotCandles(df:pd.DataFrame):
+    mpf.plot(df, type='candle', style='charles')
+
+def main():
+    df:pd.DataFrame = pd.read_csv('~/Data/20231130.csv', index_col=0, parse_dates=True)
+    # xf = combineDF(df, 5)
+
+    xf = multibars(df,5)
+
+    p1 = Process(target=PlotCandles, args=(df,))
+    p2 = Process(target=PlotCandles, args=(xf,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+if __name__ == '__main__':
+    main()
