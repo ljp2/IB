95b831bb0d30b67877944aa8efa1b0a74ef61422
ljp2 <ljp2@lehigh.edu>
2023-12-17 20:44:52 -0500

    working

--- plots.py
+++ plots.py
@@ -4,38 +4,38 @@ import matplotlib.dates as mdates
 import pandas as pd
 from multiprocessing import Process, Queue
 
+class CandleStickPlot():
+    def __init__(self, df:pd.DataFrame, title) -> None:
+        plt.ion = True
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_title(title)
+        self.ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
+        
 
-def plotCandlestick(xf: pd.DataFrame, title, q:Queue):
-    plt.ion = True
-    fig, ax = plt.subplots()
-    ax.set_title(title)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-    # plt.xticks(rotation=30, ha='right')
-
-    coldn = "red"
-    colup = "green"
-    width = (xf.index[1] - xf.index[0]) * 0.6
-    width2 = width * 0.3
-    ax.set_xlim(xf.index[0], xf.index[-1])
-    
-    plt.pause(.25)
-    
-    while True:
-        df = q.get()
-        up = df[df.Close >= df.Open]
-        down = df[df.Close < df.Open]
-
-        ax.bar(up.index, up.Close - up.Open, width, bottom=up.Open, color=colup)
-        ax.bar(up.index, up.High - up.Close, width2, bottom=up.Close, color=colup)
-        ax.bar(up.index, up.Low - up.Open, width2, bottom=up.Open, color=colup)
-
-        ax.bar(down.index, down.Close - down.Open, width, bottom=down.Open, color=coldn)
-        ax.bar(down.index, down.High - down.Open, width2, bottom=down.Open, color=coldn)
-        ax.bar(down.index, down.Low - down.Close, width2, bottom=down.Close, color=coldn)
+        self.width = (df.index[1] - df.index[0]) * 0.6
+        self.width2 = self.width * 0.3
+        self.ax.set_xlim(df.index[0], df.index[-1])
         
         plt.pause(.25)
+        
+    def addBar(self, bardf:pd.DataFrame):
+        if (bardf.Close >= bardf.Open).any():
+            self.ax.bar(bardf.index, bardf.Close - bardf.Open, self.width, bottom=bardf.Open, color="green")
+            self.ax.bar(bardf.index, bardf.High - bardf.Close, self.width2, bottom=bardf.Close, color="green")
+            self.ax.bar(bardf.index, bardf.Low - bardf.Open,self. width2, bottom=bardf.Open, color="green")
+        else:
+            self.ax.bar(bardf.index, bardf.Close - bardf.Open, self.width, bottom=bardf.Open, color= "red")
+            self.ax.bar(bardf.index, bardf.High - bardf.Open, self.width2, bottom=bardf.Open, color= "red")
+            self.ax.bar(bardf.index, bardf.Low - bardf.Close, self.width2, bottom=bardf.Close, color= "red")
+            
+        plt.pause(.25)
 
-    # plt.show()
+
+def plotCandlestick(df: pd.DataFrame, title, q:Queue):
+    candlestick_plot = CandleStickPlot(df, title)
+    while True:
+        bardf = q.get()
+        candlestick_plot.addBar(bardf=bardf)
 
 
 def plotProcess(df: pd.DataFrame, title=None):
